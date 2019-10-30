35f728d15834c832eda8e38eecb5347a4f0653ff
Kieron Gillingham <4gillk91@solent.ac.uk>
2019-10-30 12:37:43 +0000

    week 6 test/impl

---
+++ myPracticeCourseWork/week6/jpaExample1/dao-jpa/src/test/java/org/solent/com504/jpaexample1/impl/dao/jpa/test/PersonDAOTest.java
@@ -0,0 +1,197 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.solent.com504.jpaexample1.impl.dao.jpa.test;
+
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.solent.com504.jpaexample1.impl.dao.jpa.DAOFactoryJPAImpl;
+import org.solent.com504.jpaexample1.model.dao.PersonDAO;
+import org.solent.com504.jpaexample1.model.dto.Person;
+import org.solent.com504.jpaexample1.model.dto.Role;
+
+/**
+ *
+ * @author cgallen
+ */
+public class PersonDAOTest {
+
+    final static Logger LOG = LogManager.getLogger(PersonDAOTest.class);
+
+    private PersonDAO personDao = null;
+
+    private DAOFactoryJPAImpl daoFactory = new DAOFactoryJPAImpl();
+
+    @Before
+    public void before() {
+        personDao = daoFactory.getPersonDAO();
+        assertNotNull(personDao);
+    }
+
+    // initialises database for each test
+    private void init() {
+        // delete all in database
+        personDao.deleteAll();
+        // add 5 patients
+        for (int i = 1; i < 6; i++) {
+            Person p = new Person();
+            p.setAddress("address_" + i);
+            p.setFirstName("firstName_" + i);
+            p.setSecondName("secondName_" + i);
+            p.setRole(Role.PATIENT);
+            personDao.save(p);
+        }
+    }
+
+    @Test
+    public void createPersonDAOTest() {
+        LOG.debug("start of createPersonDAOTest");
+        // this test simply runs the before method
+        LOG.debug("end of createPersonDAOTest");
+    }
+
+    @Test
+    public void findByIdTest() {
+        LOG.debug("start of findByIdTest()");
+        
+        init();
+        
+        for (Person person : personDao.findAll()) {
+            Person foundPerson = personDao.findById(person.getId());
+            assertNotNull(foundPerson);
+            assertEquals(foundPerson.getFirstName(), person.getFirstName());
+            assertEquals(foundPerson.getSecondName(), person.getSecondName());
+            assertEquals(foundPerson.getAddress(), person.getAddress());
+            assertEquals(foundPerson.getRole(), person.getRole());
+        }
+        
+        LOG.debug("end of findByIdTest()");
+    }
+
+    @Test
+    public void saveTest() {
+        LOG.debug("start of saveTest()");
+        
+        init();
+        
+        Person person = new Person();
+        person.setAddress("address_6");
+        person.setFirstName("firstName_6");
+        person.setSecondName("secondName_6");
+        person.setRole(Role.PATIENT);
+        personDao.save(person);
+        
+        Person foundPerson = personDao.findById(person.getId());
+        
+        assertNotNull(foundPerson);
+        assertEquals(foundPerson.getFirstName(), person.getFirstName());
+        assertEquals(foundPerson.getSecondName(), person.getSecondName());
+        assertEquals(foundPerson.getAddress(), person.getAddress());
+        assertEquals(foundPerson.getRole(), person.getRole());
+        
+        LOG.debug("end of saveTest()");
+    }
+
+    @Test
+    public void findAllTest() {
+        LOG.debug("start of findAllTest()");
+
+        init();
+        List<Person> personList = personDao.findAll();
+        assertNotNull(personList);
+        
+        // init should insert 5 people
+        assertEquals(5, personList.size());
+
+        // print out result
+        String msg = "";
+        for (Person person : personList) {
+            msg = msg +"\n   " +  person.toString();
+        }
+        LOG.debug("findAllTest() retrieved:" + msg);
+        LOG.debug("end of findAllTest()");
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        LOG.debug("start of deleteByIdTest()");
+
+        init();
+        List<Person> personList = personDao.findAll();
+        assertNotNull(personList);
+        
+        assertEquals(5, personList.size());
+        
+        for (int i = 1; i < 6; i++) { 
+            Person person = personList.get(i-1);
+            assertNotNull(person);
+            Long id = person.getId();
+
+            personDao.deleteById(id);
+            
+            assertNull(personDao.findById(id));
+            assertEquals(5 - i, personDao.findAll().size());
+        }
+        
+        LOG.debug("end of deleteByIdTest()");
+    }
+
+    
+    // Incomplete
+    @Test
+    public void deleteTest() {
+        LOG.debug("start of deleteTest()");
+        
+        init();
+        
+        List<Person> personList = personDao.findAll();
+        assertNotNull(personList);
+        
+        assertEquals(5, personList.size());
+        
+        for (int i = 1; i < 6; i++) { 
+            Person person = personList.get(i-1);
+            assertNotNull(person);
+            Long id = person.getId();
+
+            personDao.deleteById(id);
+            
+            assertNull(personDao.findById(id));
+            assertEquals(5 - i, personDao.findAll().size());
+        }
+        
+        
+        LOG.debug("end of deleteTest()");
+    }
+
+    @Test
+    public void deleteAllTest() {
+        LOG.debug("start of deleteAllTest())");
+        //TODO implement test
+        LOG.debug("NOT IMPLEMENTED");
+        LOG.debug("end of deleteAllTest()");
+    }
+
+    @Test
+    public void findByRoleTest() {
+        LOG.debug("start of findByIdTest()");
+        //TODO implement test
+        LOG.debug("NOT IMPLEMENTED");
+        LOG.debug("end of findByIdTest()");
+    }
+
+    @Test
+    public void findByNameTest() {
+        LOG.debug("start of findByNameTest()");
+        //TODO implement test
+        LOG.debug("NOT IMPLEMENTED");
+        LOG.debug("end of findByNameTest())");
+
+    }
+}
