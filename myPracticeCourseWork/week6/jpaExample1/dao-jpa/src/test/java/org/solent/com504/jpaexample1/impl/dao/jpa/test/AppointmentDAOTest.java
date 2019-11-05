a88810a3bb8733fae6de34ae312a8c0b6e54f109
Kieron Gillingham <kierongillingham@gmail.com>
2019-11-05 19:18:33 +0000

    jpaImpl and testing

--- myPracticeCourseWork/week6/jpaExample1/dao-jpa/src/test/java/org/solent/com504/jpaexample1/impl/dao/jpa/test/AppointmentDAOTest.java
+++ myPracticeCourseWork/week6/jpaExample1/dao-jpa/src/test/java/org/solent/com504/jpaexample1/impl/dao/jpa/test/AppointmentDAOTest.java
@@ -5,13 +5,23 @@
  */
 package org.solent.com504.jpaexample1.impl.dao.jpa.test;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.solent.com504.jpaexample1.impl.dao.jpa.AppointmentDAOJpaImpl;
 import org.solent.com504.jpaexample1.impl.dao.jpa.DAOFactoryJPAImpl;
+import static org.solent.com504.jpaexample1.impl.dao.jpa.test.PersonDAOTest.LOG;
 import org.solent.com504.jpaexample1.model.dao.AppointmentDAO;
+import org.solent.com504.jpaexample1.model.dto.Appointment;
+import org.solent.com504.jpaexample1.model.dto.Person;
+import org.solent.com504.jpaexample1.model.dto.Role;
 
 
 /**
@@ -26,6 +36,44 @@ public class AppointmentDAOTest {
 
     private DAOFactoryJPAImpl daoFactory = new DAOFactoryJPAImpl();
 
+    private List<Person> people = new ArrayList<>();
+    
+    // initialises database for each test
+    private void init() {
+        // delete all in database
+        String msg = "Init():";
+        appointmentDao.deleteAll();
+        people.clear();
+        
+        for (int i = 1; i < 6; i++) {
+            Person pat = new Person();
+            pat.setFirstName("pat_" + i);
+            pat.setRole(Role.PATIENT);
+            people.add(pat);
+            
+            Person den = new Person();
+            den.setFirstName("den_" + i);
+            den.setRole(Role.DENTIST);
+            people.add(den);
+        }
+        
+        msg = msg + "\n   Cleared all appointments.";
+        // add 5 patients
+        for (int i = 1; i < 6; i++) {
+            Appointment a = new Appointment();
+            a.setDescripton("description_" + i);
+            a.setDurationMinutes(i * 10);
+            a.setHr(12 + i);
+            a.setMth(i);
+            a.setPersonA(people.get((2 * i) - 1));
+            a.setPersonB(people.get(2 * i));
+            a.setYr(2000 + i);
+            appointmentDao.save(a);
+            msg = msg + "\n   Created " + a.toString();
+        }
+        LOG.debug(msg);
+    }
+    
     @Before
     public void before() {
         appointmentDao = daoFactory.getAppointmentDAO();
@@ -39,10 +87,159 @@ public class AppointmentDAOTest {
         LOG.debug("end of createAppointmentDAOTest(");
     }
     
-    
-    
-    
-    
-    
+    /**
+     * Test of findById method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testFindById() {
+        LOG.debug("start of findByIdTest()");
+        
+        init();
+        
+        /*
+        String msg = "";
+        
+        for (Person person : personDao.findAll()) {
+            Person foundPerson = personDao.findById(person.getId());
+            assertNotNull(foundPerson);
+            msg = msg +"\n   Comparing target: " + person.toString() + " with returned: " + foundPerson.toString();
+            assertEquals(foundPerson.getFirstName(), person.getFirstName());
+            assertEquals(foundPerson.getSecondName(), person.getSecondName());
+            assertEquals(foundPerson.getAddress(), person.getAddress());
+            assertEquals(foundPerson.getRole(), person.getRole());
+        }
+        LOG.debug("findByIdTest():" + msg);
+        LOG.debug("end of findByIdTest()");
+        
+        
+        System.out.println("findById");
+        Long id = null;
+        AppointmentDAOJpaImpl instance = null;
+        Appointment expResult = null;
+        Appointment result = instance.findById(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        */
+    }
+
+    /**
+     * Test of save method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testSave() {
+        System.out.println("save");
+        Appointment appointment = null;
+        AppointmentDAOJpaImpl instance = null;
+        Appointment expResult = null;
+        Appointment result = instance.save(appointment);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findAll method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testFindAll() {
+        System.out.println("findAll");
+        AppointmentDAOJpaImpl instance = null;
+        List<Appointment> expResult = null;
+        List<Appointment> result = instance.findAll();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of delete method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testDelete() {
+        System.out.println("delete");
+        Appointment appointment = null;
+        AppointmentDAOJpaImpl instance = null;
+        Appointment expResult = null;
+        Appointment result = instance.delete(appointment);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteById method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testDeleteById() {
+        System.out.println("deleteById");
+        Long id = null;
+        AppointmentDAOJpaImpl instance = null;
+        instance.deleteById(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteAll method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testDeleteAll() {
+        System.out.println("deleteAll");
+        AppointmentDAOJpaImpl instance = null;
+        instance.deleteAll();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findByPersonA method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testFindByPersonA() {
+        System.out.println("findByPersonA");
+        Person personA = null;
+        AppointmentDAOJpaImpl instance = null;
+        List<Appointment> expResult = null;
+        List<Appointment> result = instance.findByPersonA(personA);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findByPersonB method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testFindByPersonB() {
+        System.out.println("findByPersonB");
+        Person personB = null;
+        AppointmentDAOJpaImpl instance = null;
+        List<Appointment> expResult = null;
+        List<Appointment> result = instance.findByPersonB(personB);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findByDate method, of class AppointmentDAOJpa.
+     */
+    @Test
+    public void testFindByDate() {
+        System.out.println("findByDate");
+        Integer year = null;
+        Integer month = null;
+        Integer hour = null;
+        Integer minutes = null;
+        AppointmentDAOJpaImpl instance = null;
+        List<Appointment> expResult = null;
+        List<Appointment> result = instance.findByDate(year, month, hour, minutes);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
     
 }
--- myPracticeCourseWork/week6/jpaExample1/dao-jpa/src/test/java/org/solent/com504/jpaexample1/impl/dao/jpa/test/PersonDAOTest.java
+++ myPracticeCourseWork/week6/jpaExample1/dao-jpa/src/test/java/org/solent/com504/jpaexample1/impl/dao/jpa/test/PersonDAOTest.java
@@ -37,7 +37,10 @@ public class PersonDAOTest {
     // initialises database for each test
     private void init() {
         // delete all in database
+        String msg = "Init():";
         personDao.deleteAll();
+        
+        msg = msg + "\n   Cleared all people.";
         // add 5 patients
         for (int i = 1; i < 6; i++) {
             Person p = new Person();
@@ -46,7 +49,10 @@ public class PersonDAOTest {
             p.setSecondName("secondName_" + i);
             p.setRole(Role.PATIENT);
             personDao.save(p);
+            msg = msg + "\n   Created " + p.toString();
+            
         }
+        LOG.debug(msg);
     }
 
     @Test
@@ -59,16 +65,47 @@ public class PersonDAOTest {
     @Test
     public void findByIdTest() {
         LOG.debug("start of findByIdTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
+        
+        init();
+        String msg = "";
+        
+        for (Person person : personDao.findAll()) {
+            Person foundPerson = personDao.findById(person.getId());
+            assertNotNull(foundPerson);
+            msg = msg +"\n   Comparing target: " + person.toString() + " with returned: " + foundPerson.toString();
+            assertEquals(foundPerson.getFirstName(), person.getFirstName());
+            assertEquals(foundPerson.getSecondName(), person.getSecondName());
+            assertEquals(foundPerson.getAddress(), person.getAddress());
+            assertEquals(foundPerson.getRole(), person.getRole());
+        }
+        LOG.debug("findByIdTest():" + msg);
         LOG.debug("end of findByIdTest()");
     }
 
     @Test
     public void saveTest() {
         LOG.debug("start of saveTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
+        
+        init();
+        
+        Person person = new Person();
+        person.setAddress("address_6");
+        person.setFirstName("firstName_6");
+        person.setSecondName("secondName_6");
+        person.setRole(Role.PATIENT);
+        personDao.save(person);
+        LOG.debug("Created " + person.toString());
+        
+        LOG.debug("Retrieving " + person.toString() + " from database.");
+        Person foundPerson = personDao.findById(person.getId());
+        
+        assertNotNull(foundPerson);
+        LOG.debug("Comparing target: " + person.toString() + " with returned: " + foundPerson.toString());
+        assertEquals(foundPerson.getFirstName(), person.getFirstName());
+        assertEquals(foundPerson.getSecondName(), person.getSecondName());
+        assertEquals(foundPerson.getAddress(), person.getAddress());
+        assertEquals(foundPerson.getRole(), person.getRole());
+        
         LOG.debug("end of saveTest()");
     }
 
@@ -78,7 +115,7 @@ public class PersonDAOTest {
 
         init();
         List<Person> personList = personDao.findAll();
-        assertNotNull(personList);
+        assertFalse(personList.isEmpty());
         
         // init should insert 5 people
         assertEquals(5, personList.size());
@@ -89,48 +126,138 @@ public class PersonDAOTest {
             msg = msg +"\n   " +  person.toString();
         }
         LOG.debug("findAllTest() retrieved:" + msg);
-
-        LOG.debug("NOT IMPLEMENTED");
         LOG.debug("end of findAllTest()");
     }
 
     @Test
     public void deleteByIdTest() {
         LOG.debug("start of deleteByIdTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
+
+        init();
+        List<Person> personList = personDao.findAll();
+        assertFalse(personList.isEmpty());
+        
+        assertEquals(5, personList.size());
+        
+        String msg = "";
+        
+        for (int i = 1; i < 6; i++) { 
+            Person person = personList.get(i-1);
+            assertNotNull(person);
+            Long id = person.getId();
+
+            personDao.deleteById(id);
+            
+            assertNull(personDao.findById(id));
+            assertEquals(5 - i, personDao.findAll().size());
+            
+            msg = msg +"\n   Removed: " +  person.toString();
+        }
+        LOG.debug("deleteByIdTest():" + msg);
         LOG.debug("end of deleteByIdTest()");
     }
 
     @Test
     public void deleteTest() {
         LOG.debug("start of deleteTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
-        LOG.debug("end ofdeleteTest()");
+        
+        init();
+        
+        List<Person> personList = personDao.findAll();
+        assertNotNull(personList);
+        
+        assertEquals(5, personList.size());
+        String msg = "";
+        
+        for (int i = 1; i < 6; i++) { 
+            Person person = personList.get(i-1);
+            assertNotNull(person);
+            Long id = person.getId();
+            
+            personDao.delete(person);
+            
+            assertNull(personDao.findById(id));
+            assertEquals(5 - i, personDao.findAll().size());
+            
+            msg = msg +"\n   Removed: " +  person.toString();
+        }
+        LOG.debug("deleteTest():" + msg);
+        
+        LOG.debug("end of deleteTest()");
     }
 
     @Test
     public void deleteAllTest() {
         LOG.debug("start of deleteAllTest())");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
+        
+        init();
+        assertNotNull(personDao.findAll());
+        
+        personDao.deleteAll();
+        
+        assertTrue(personDao.findAll().isEmpty());
+        LOG.debug("deleteAllTest: Cleared all people.");
+        
         LOG.debug("end of deleteAllTest()");
     }
 
     @Test
     public void findByRoleTest() {
-        LOG.debug("start of findByIdTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
+        LOG.debug("start of findByRoleTest()");
+     
+        init();
+        Person person = new Person();
+        person.setAddress("address_6");
+        person.setFirstName("firstName_6");
+        person.setSecondName("secondName_6");
+        person.setRole(Role.DENTIST);
+        personDao.save(person);
+        LOG.debug("Created " + person.toString());
+        
+        
+        String msg = "";
+        
+        List<Person> patients = personDao.findByRole(Role.PATIENT);
+        assertFalse(patients.isEmpty());
+        msg = msg + "findByRole(Role.PATIENT) returned:";
+        
+        for (Person p : patients) {
+            assertEquals(p.getRole(), Role.PATIENT);
+            msg = msg + "\n  " + p.toString();
+        }
+        
+        List<Person> dentists = personDao.findByRole(Role.DENTIST);
+        assertFalse(dentists.isEmpty());
+        msg = msg + "\nfindByRole(Role.DENTIST) returned:";
+        
+        for (Person p : dentists) {
+            assertEquals(p.getRole(), Role.DENTIST);
+            msg = msg + "\n  " + p.toString();
+        }
+        
+        LOG.debug("findByRoleTest(): returned: " + msg);
+
         LOG.debug("end of findByIdTest()");
     }
 
     @Test
     public void findByNameTest() {
         LOG.debug("start of findByNameTest()");
-        //TODO implement test
-        LOG.debug("NOT IMPLEMENTED");
+        
+        init();
+        String msg = "";
+        
+        for (Person person : personDao.findAll()) {
+            List<Person> results = personDao.findByName(person.getFirstName(), person.getSecondName());
+            assertEquals(results.size(), 1);
+            Person foundPerson = results.get(0);
+            msg = msg +"\n   Comparing target: " + person.toString() + " with returned: " + foundPerson.toString();
+            assertEquals(foundPerson.getFirstName(), person.getFirstName());
+            assertEquals(foundPerson.getSecondName(), person.getSecondName());
+            assertEquals(foundPerson.getAddress(), person.getAddress());
+            assertEquals(foundPerson.getRole(), person.getRole());
+        }
+        LOG.debug("findByNameTest():" + msg);
         LOG.debug("end of findByNameTest())");
 
     }
